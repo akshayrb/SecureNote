ff10f60e7da6d2631d246598b476bf894d47bd54
Akshay R B <axay.rb@gmail.com>
2014-09-09 22:57:37 +0530

    final

---
+++ SEP/src/seproject/AutoSave.java
@@ -0,0 +1,38 @@
+package seproject;
+
+
+import java.util.TimerTask;
+import javax.swing.JTextArea;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class AutoSave extends TimerTask {
+    //times member represent calling times.
+    private int times = 0;
+	 public JTextArea t;
+	 String content;
+AutoSave(JTextArea t,String content)
+ {
+ this.t=t;
+ this.content=content;
+ }
+    public void run() {
+  		while(times==0)
+		  try
+        {
+            FileWriter fw = new FileWriter("C:/SecureNote/AutoSave.txt");
+            fw.write(t.getText());
+            
+            fw.close();
+        }
+        catch(IOException i)
+        {
+
+        }
+
+            //Stop Timer.
+            this.cancel();
+
+
+}
+}
